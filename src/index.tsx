7556ccf937e1d8abccb435e246c41e591b065e94
Sergey Koshelev <shivacharya2019@gmail.com>
2023-04-28 17:20:16 +0300

    add store from redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <React.StrictMode> */}
+      <App />
+      {/* </React.StrictMode> */}
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
