b5168f4829bfed8458e151e983aeeb1221310e2a
Sergey Koshelev <shivacharya2019@gmail.com>
2023-05-01 20:00:24 +0300

    upd import

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Notifications } from '@mantine/notifications';
-import { AppShell, Header, Footer } from '@mantine/core';
-import { HeaderMenu } from './components/headerMenu/headerMenu';
-import { BooksList } from './pages/catalog/BooksList';
-import { Error404 } from './pages/error404/error404';
-import { useAutoLogin } from './hooks/useAutoLogin';
-import { LINKS } from './common/constants';
-import UserAccount from './pages/userAccount/UserAccount';
-
-import CustomScrollbar from './components/customScrollbar/CustomScrollbar';
-import { Home } from './pages/home/home';
-import { Cart } from './pages/cart/cart';
-import { Favorites } from './pages/favorites/favorites';
-import { Stocks } from './pages/stocks/stocks';
-import { BookstoreServices } from './pages/bookstoreServices/bookstoreServices';
-import { ReaderBlogs } from './pages/readerBlogs/readerBlogs';
-import { AboutUs } from './pages/aboutUs/aboutUs';
-import ChatBot from './components/chatBot/ChatBot';
-
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Notifications } from "@mantine/notifications";
+import { AppShell, Header, Footer } from "@mantine/core";
+import { HeaderMenu } from "./components/headerMenu/headerMenu";
+import { BooksList } from "./pages/catalog/BooksList";
+import { Error404 } from "./pages/error404/error404";
+import { useAutoLogin } from "./hooks/useAutoLogin";
+import { LINKS } from "./common/constants";
+import UserAccount from "./pages/userAccount/UserAccount";
+import CustomScrollbar from "./components/customScrollbar/CustomScrollbar";
+import ChatBot from "./components/chatBot/ChatBot";
+import { AboutUs } from "./pages/aboutUs/AboutUs";
+import { BookstoreServices } from "./pages/bookstoreServices/BookstoreServices";
+import { Cart } from "./pages/cart/Cart";
+import { Favorites } from "./pages/favorites/Favorites";
+import { Home } from "./pages/home/Home";
+import { ReaderBlogs } from "./pages/readerBlogs/ReaderBlogs";
+import { Stocks } from "./pages/stocks/stocks";
 
 export default function App() {
   useAutoLogin();
@@ -49,22 +50,24 @@ export default function App() {
             <Footer height={60} p="md">
               © 2023 World of books. All rights reserved.
             </Footer>
-
-          }>
+          }
+        >
           <ChatBot />
           <CustomScrollbar>
             <Routes>
-              <Route path={'/'} element={<Home />} />
-              <Route path={'/cart'} element={<Cart />} />
-              <Route path={'/favorites'} element={<Favorites />} />
-              <Route path={'/stocks'} element={<Stocks />} />
-              <Route path={'/bookstoreServices'} element={<BookstoreServices />} />
-              <Route path={'/readerBlogs'} element={<ReaderBlogs />} />
-              <Route path={'/aboutUs'} element={<AboutUs />} />
-              <Route path={'/BooksList'} element={<BooksList />} />
-              <Route path={'/user-account'} element={<UserAccount />} />
-              <Route path={'/*'} element={<Error404 />} />
-
+              <Route path={"/"} element={<Home />} />
+              <Route path={"/cart"} element={<Cart />} />
+              <Route path={"/favorites"} element={<Favorites />} />
+              <Route path={"/stocks"} element={<Stocks />} />
+              <Route
+                path={"/bookstoreServices"}
+                element={<BookstoreServices />}
+              />
+              <Route path={"/readerBlogs"} element={<ReaderBlogs />} />
+              <Route path={"/aboutUs"} element={<AboutUs />} />
+              <Route path={"/BooksList"} element={<BooksList />} />
+              <Route path={"/user-account"} element={<UserAccount />} />
+              <Route path={"/*"} element={<Error404 />} />
             </Routes>
           </CustomScrollbar>
         </AppShell>
