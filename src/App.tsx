eefdfc0f30e4cbe0bcf8500a06ed6314aa753b93
ds4045 <ds4045@mail.ru>
2023-05-01 13:58:02 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 63c59a4152d6253836a0648cfafde97d9bf212f5
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
@@ -14,7 +10,6 @@ import { useAutoLogin } from './hooks/useAutoLogin';
 import { LINKS } from './common/constants';
 import UserAccount from './pages/userAccount/UserAccount';
 
-<<<<<<< HEAD
 import CustomScrollbar from './components/customScrollbar/CustomScrollbar';
 import { Home } from './pages/home/home';
 import { Cart } from './pages/cart/cart';
@@ -23,8 +18,6 @@ import { Stocks } from './pages/stocks/stocks';
 import { BookstoreServices } from './pages/bookstoreServices/bookstoreServices';
 import { ReaderBlogs } from './pages/readerBlogs/readerBlogs';
 import { AboutUs } from './pages/aboutUs/aboutUs';
-=======
->>>>>>> 63c59a4152d6253836a0648cfafde97d9bf212f5
 
 export default function App() {
   useAutoLogin();
@@ -47,7 +40,6 @@ export default function App() {
               © 2023 World of books. All rights reserved.
             </Footer>
           }>
-<<<<<<< HEAD
           <CustomScrollbar>
             <Routes>
               <Route path={'/'} element={<Home />} />
@@ -62,22 +54,6 @@ export default function App() {
               <Route path={'/*'} element={<Error404 />} />
             </Routes>
           </CustomScrollbar>
-=======
-          <Routes>
-
-            <Route path={'/'} element={<Home />} />
-            <Route path={'/cart'} element={<Cart />} />
-            <Route path={'/favorites'} element={<Favorites />} />
-            <Route path={'/stocks'} element={<Stocks />} />
-            <Route path={'/bookstoreServices'} element={<BookstoreServices />} />
-            <Route path={'/readerBlogs'} element={<ReaderBlogs />} />
-            <Route path={'/aboutUs'} element={<AboutUs />} />
-            <Route path={'/BooksList'} element={<BooksList />} />
-            <Route path={'/user-account'} element={<UserAccount />} />
-            <Route path={'/*'} element={<Error404 />} />
-
-          </Routes>
->>>>>>> 63c59a4152d6253836a0648cfafde97d9bf212f5
         </AppShell>
       </MantineProvider>
     </ColorSchemeProvider>
