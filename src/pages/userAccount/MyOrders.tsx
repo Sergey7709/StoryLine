d7d91fc1565e2aa5ff4263d438e1a4220ad09f98
ds4045 <ds4045@mail.ru>
2023-05-04 23:13:54 +0300

    fix bugs

--- src/pages/userAccount/MyOrders.tsx
+++ src/pages/userAccount/MyOrders.tsx
@@ -1,23 +1,16 @@
 import { Table } from '@mantine/core';
-import { books } from '../catalog/BooksList';
 import ShowOrderedItems from './assetsUserAccount/ShowOrderedItems';
-
-const elements = [
-  { id: 6, totalPrice: 12.011, date: '10.10.22', itemId: ['Carbon', 'Carbon1'] },
-  { id: 7, totalPrice: 14.007, date: '10.10.22', itemId: ['Nitrogen'] },
-  { id: 39, totalPrice: 88.906, date: '10.10.22', itemId: ['Yttrium'] },
-  { id: 56, totalPrice: 137.33, date: '10.10.22', itemId: ['Barium'] },
-  { id: 58, totalPrice: 140.12, date: '10.10.22', itemId: ['Cerium'] },
-];
+import { useAppSelector } from '../../redux/redux.hooks';
 
 const MyOrders = () => {
-  const rows = elements.map((el) => (
+  const user = useAppSelector((state) => state.auth.user);
+  const rows = user?.orderItems.map((el) => (
     <tr key={el.id}>
       <td>{el.id}</td>
       <td>{el.totalPrice} &#8381;</td>
       <td>{el.date}</td>
       <td>
-        <ShowOrderedItems books={books[0]} itemId={el.itemId} />
+        <ShowOrderedItems books={el.items} />
       </td>
     </tr>
   ));
