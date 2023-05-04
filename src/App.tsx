faa4a39c8dcefb0a9dcebe8f3dd17a278ffca122
ds4045 <ds4045@mail.ru>
2023-05-04 12:28:15 +0300

    fix types Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,35 @@
-import {
-  MantineProvider,
-  ColorSchemeProvider,
-  ColorScheme,
-} from "@mantine/core";
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Notifications } from "@mantine/notifications";
-import { AppShell, Header, Footer } from "@mantine/core";
-import { HeaderMenu } from "./components/headerMenu/headerMenu";
-import { BooksList } from "./pages/catalog/BooksList";
-import { Error404 } from "./pages/error404/error404";
-import { useAutoLogin } from "./hooks/useAutoLogin";
-import { LINKS } from "./common/constants";
-import UserAccount from "./pages/userAccount/UserAccount";
-import CustomScrollbar from "./components/customScrollbar/CustomScrollbar";
-import { ScrollToTopButton } from "./components/scroll-to-top-button/ScrollToTopButton";
-import ChatBot from "./components/chatBot/ChatBot";
-import { AboutUs } from "./pages/aboutUs/AboutUs";
-import { BookstoreServices } from "./pages/bookstoreServices/BookstoreServices";
-import { Cart } from "./pages/cart/Cart";
-import { Favorites } from "./pages/favorites/Favorites";
-import { Home } from "./pages/home/Home";
-import { ReaderBlogs } from "./pages/readerBlogs/ReaderBlogs";
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Notifications } from '@mantine/notifications';
+import { AppShell, Header, Footer } from '@mantine/core';
+import { HeaderMenu } from './components/headerMenu/headerMenu';
+import { BooksList } from './pages/catalog/BooksList';
+import { Error404 } from './pages/error404/error404';
+import { useAutoLogin } from './hooks/useAutoLogin';
+import { LINKS } from './common/constants';
+import UserAccount from './pages/userAccount/UserAccount';
+import CustomScrollbar from './components/customScrollbar/CustomScrollbar';
+import { ScrollToTopButton } from './components/scroll-to-top-button/ScrollToTopButton';
+import ChatBot from './components/chatBot/ChatBot';
 
-import { Stocks } from "./pages/stocks/stocks";
+import { Stocks } from './pages/stocks/stocks';
+import { Home } from './pages/home/home';
+import { Cart } from './pages/cart/cart';
+import { Favorites } from './pages/favorites/favorites';
+import { BookstoreServices } from './pages/bookstoreServices/bookstoreServices';
+import { ReaderBlogs } from './pages/readerBlogs/readerBlogs';
+import { AboutUs } from './pages/aboutUs/aboutUs';
 
 export default function App() {
   useAutoLogin();
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{ colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
         <Notifications />
         <AppShell
           header={
@@ -52,25 +41,21 @@ export default function App() {
             <Footer height={60} p="md">
               © 2023 World of books. All rights reserved.
             </Footer>
-          }
-        >
+          }>
           <ChatBot />
 
           <CustomScrollbar>
             <Routes>
-              <Route path={"/"} element={<Home />} />
-              <Route path={"/cart"} element={<Cart />} />
-              <Route path={"/favorites"} element={<Favorites />} />
-              <Route path={"/stocks"} element={<Stocks />} />
-              <Route
-                path={"/bookstoreServices"}
-                element={<BookstoreServices />}
-              />
-              <Route path={"/readerBlogs"} element={<ReaderBlogs />} />
-              <Route path={"/aboutUs"} element={<AboutUs />} />
-              <Route path={"/BooksList"} element={<BooksList />} />
-              <Route path={"/user-account"} element={<UserAccount />} />
-              <Route path={"/*"} element={<Error404 />} />
+              <Route path={'/'} element={<Home />} />
+              <Route path={'/cart'} element={<Cart />} />
+              <Route path={'/favorites'} element={<Favorites />} />
+              <Route path={'/stocks'} element={<Stocks />} />
+              <Route path={'/bookstoreServices'} element={<BookstoreServices />} />
+              <Route path={'/readerBlogs'} element={<ReaderBlogs />} />
+              <Route path={'/aboutUs'} element={<AboutUs />} />
+              <Route path={'/BooksList'} element={<BooksList />} />
+              <Route path={'/user-account'} element={<UserAccount />} />
+              <Route path={'/*'} element={<Error404 />} />
             </Routes>
 
             <ScrollToTopButton />
