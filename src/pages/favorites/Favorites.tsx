63d87ddb3ac7bef102d114b4f84023002f46b3b6
Sergey Koshelev <shivacharya2019@gmail.com>
2023-05-27 20:16:09 +0300

    edit Favorites fix optimize render

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -8,9 +8,10 @@ import { Divider, Flex, Grid, Space } from "@mantine/core";
 import SingleBookList from "../catalog/SingleBookList";
 import EmptyData from "../userAccount/assetsUserAccount/EmptyData";
 import { Title } from "@mantine/core";
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
+import React from "react";
 
-export const Favorites = () => {
+export const Favorites = React.memo(() => {
   const user = useAppSelector((state) => state.auth.user);
 
   const getCurrentUser = useCurrentUser();
@@ -31,7 +32,6 @@ export const Favorites = () => {
       onSuccess: () => {
         getCurrentUser();
       },
-
       onError: () => {
         notifications.show({
           message: "Ошибка при добавлении книги в избранное!",
@@ -42,46 +42,49 @@ export const Favorites = () => {
     }
   );
 
-  const favoritesChange = async (bookId: number, favorite: boolean) => {
-    if (!user) {
-      notifications.show({
-        message: "Войдите в аккаунт, что бы добавить книгу в избранное!",
-        autoClose: 5000,
-        color: "red",
-        fz: "md",
-      });
-
-      return;
-    }
-
-    if (user?.token) {
-      if (favorite === false) {
-        await mutateAsync(`user/favorites/${bookId}`);
-
+  const favoritesChange = useCallback(
+    async (bookId: number, favorite: boolean) => {
+      if (!user) {
         notifications.show({
-          message: "Книга добавлена в избранное",
-          autoClose: 2000,
-          color: "green",
+          message: "Войдите в аккаунт, что бы добавить книгу в избранное!",
+          autoClose: 5000,
+          color: "red",
+          fz: "md",
         });
-      } else if (favorite === true) {
-        await mutateAsync(`user/favorites-remove/${bookId}`);
-
-        notifications.show({
-          message: "Книга удалена из избранного",
-          autoClose: 2000,
-          color: "yellow",
-        });
+        return;
       }
-    }
-    // getCurrentUser();
-  };
+
+      if (user?.token) {
+        if (favorite === false) {
+          await mutateAsync(`user/favorites/${bookId}`);
+
+          notifications.show({
+            message: "Книга добавлена в избранное",
+            autoClose: 2000,
+            color: "green",
+          });
+        } else if (favorite === true) {
+          await mutateAsync(`user/favorites-remove/${bookId}`);
+
+          notifications.show({
+            message: "Книга удалена из избранного",
+            autoClose: 2000,
+            color: "yellow",
+          });
+        }
+      }
+    },
+    [user?.favoriteItems]
+  );
+
+  const favoriteItems = useMemo(
+    () => user?.favoriteItems ?? [],
+    [user?.favoriteItems]
+  );
 
   return (
     <>
-      {!user && (
-        <EmptyData text="Зарегистрируйтесь или авторизируйтесь для добавления книг в избранное" />
-      )}
-
+      {!user && <></>}
       {user && (
         <>
           <Flex justify={"center"} align={"center"}>
@@ -96,9 +99,9 @@ export const Favorites = () => {
           </Flex>
           <Divider size="xs" variant="solid" color="gray" />
           <Space h="md" />
-          {user?.favoriteItems.length ? (
+          {favoriteItems.length ? (
             <Grid pl={5}>
-              {user?.favoriteItems.map((favoriteBook) => (
+              {favoriteItems.map((favoriteBook) => (
                 <SingleBookList
                   key={favoriteBook.id}
                   book={favoriteBook}
@@ -114,4 +117,110 @@ export const Favorites = () => {
       )}
     </>
   );
-};
+});
+
+// export const Favorites = () => {
+//   const user = useAppSelector((state) => state.auth.user);
+
+//   const getCurrentUser = useCurrentUser();
+
+//   useEffect(() => {
+//     getCurrentUser();
+//   }, []);
+
+//   const { mutateAsync } = useMutation(
+//     (param: string) => {
+//       return axios.post(`${BASE_URL}${param}`, undefined, {
+//         headers: {
+//           Authorization: user?.token ?? "",
+//         },
+//       });
+//     },
+//     {
+//       onSuccess: () => {
+//         getCurrentUser();
+//       },
+
+//       onError: () => {
+//         notifications.show({
+//           message: "Ошибка при добавлении книги в избранное!",
+//           autoClose: 2000,
+//           color: "red",
+//         });
+//       },
+//     }
+//   );
+
+//   const favoritesChange = async (bookId: number, favorite: boolean) => {
+//     if (!user) {
+//       notifications.show({
+//         message: "Войдите в аккаунт, что бы добавить книгу в избранное!",
+//         autoClose: 5000,
+//         color: "red",
+//         fz: "md",
+//       });
+
+//       return;
+//     }
+
+//     if (user?.token) {
+//       if (favorite === false) {
+//         await mutateAsync(`user/favorites/${bookId}`);
+
+//         notifications.show({
+//           message: "Книга добавлена в избранное",
+//           autoClose: 2000,
+//           color: "green",
+//         });
+//       } else if (favorite === true) {
+//         await mutateAsync(`user/favorites-remove/${bookId}`);
+
+//         notifications.show({
+//           message: "Книга удалена из избранного",
+//           autoClose: 2000,
+//           color: "yellow",
+//         });
+//       }
+//     }
+//     // getCurrentUser();
+//   };
+
+//   return (
+//     <>
+//       {!user && (
+//         <EmptyData text="Зарегистрируйтесь или авторизируйтесь для добавления книг в избранное" />
+//       )}
+
+//       {user && (
+//         <>
+//           <Flex justify={"center"} align={"center"}>
+//             <Title
+//               pb={"sm"}
+//               variant="gradient"
+//               gradient={{ from: "indigo", to: "green", deg: 45 }}
+//               order={1}
+//             >
+//               МОИ ИЗБРАННЫЕ КНИГИ
+//             </Title>
+//           </Flex>
+//           <Divider size="xs" variant="solid" color="gray" />
+//           <Space h="md" />
+//           {user?.favoriteItems.length ? (
+//             <Grid pl={5}>
+//               {user?.favoriteItems.map((favoriteBook) => (
+//                 <SingleBookList
+//                   key={favoriteBook.id}
+//                   book={favoriteBook}
+//                   favorite={true}
+//                   favoritesChange={favoritesChange}
+//                 />
+//               ))}
+//             </Grid>
+//           ) : (
+//             <EmptyData text="У вас нет избранных товаров" />
+//           )}
+//         </>
+//       )}
+//     </>
+//   );
+// };
